b371a815ac599a6b0d6170da710dbb6f58a96402
valehaedo <valeriahaedo@gmail.com>
2020-08-01 21:32:03 +0200

    end of proyect

--- proyectreact/src/App.js
+++ proyectreact/src/App.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Tweet from "./Tweet";
+
 
 
 function App() {
-
-  
+ const [users, setUsers] = useState([
+   {name:"Susy", message: "We need to clean everithing" },
+   {name:"Juanu", message: "We need to program everithing" },
+   {name: "Vale", message: "We need to do what we need to do"}
+ ])
   
   return (
-    <div>
-    <h1> This is the app component</h1>
-    
-  </div>
-);
+    <div className="app">
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message}/>
+      ))}
+    </div>
+  );
 
 };
 
